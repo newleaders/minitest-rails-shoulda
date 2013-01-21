f9fcd2f53189d54f8b9b42a1c267350f928f725d
David White <david@spry-soft.com>
2013-01-21 07:43:29 +0000

    Fixed require bug to work with latest version of minitest-rails gem

--- lib/minitest/rails/shoulda/matchers.rb
+++ lib/minitest/rails/shoulda/matchers.rb
@@ -1,7 +1,7 @@
 require "minitest/matchers"
+require "minitest/rails"
 
 if defined?(ActiveRecord)
-  require "minitest/rails/active_support"
   require "shoulda/matchers/active_record"
 
   Shoulda::Matchers::ActiveRecord.module_eval do
@@ -18,7 +18,6 @@ if defined?(ActiveRecord)
 end
 
 if defined?(ActiveModel)
-  require "minitest/rails/active_support"
   require "shoulda/matchers/active_model"
 
   Shoulda::Matchers::ActiveModel.module_eval do
@@ -35,7 +34,6 @@ if defined?(ActiveModel)
 end
 
 if defined?(ActionController)
-  require "minitest/rails/action_controller"
   require "shoulda/matchers/action_controller"
 
   Shoulda::Matchers::ActionController.module_eval do
@@ -52,7 +50,6 @@ if defined?(ActionController)
 end
 
 if defined?(ActionMailer)
-  require "minitest/rails/action_mailer"
   require "shoulda/matchers/action_mailer"
 
   Shoulda::Matchers::ActionMailer.module_eval do
