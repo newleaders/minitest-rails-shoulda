7b2ef4f2aaf34ad7c5d01db425b82c0794b37c0f
Esteban Pastorino <ejpastorino@gmail.com>
2013-04-11 20:10:35 -0300

    change MiniTest::Spec monkeypatching to MiniTest::Spec::DSL. That is now extended in ActiveSupport::TestCase

--- lib/minitest/rails/shoulda/dsl.rb
+++ lib/minitest/rails/shoulda/dsl.rb
@@ -1,28 +1,26 @@
 require "minitest/spec"
 
-# Duck punch MiniTest::Spec to behave more like Shoulda
+# Duck punch MiniTest::Spec::DSL to behave more like Shoulda
+module MiniTest::Spec::DSL
 
-class MiniTest::Spec < MiniTest::Unit::TestCase
-  class << self
+  # Add Shoulda DSL
+  alias :setup    :before
+  alias :teardown :after
+  alias :context  :describe
+  alias :should   :it
 
-    # Add Shoulda DSL
-    alias :setup    :before
-    alias :teardown :after
-    alias :context  :describe
-    alias :should   :it
+  ##
+  # Define a named expectation to be skipped for now.
+  # The implementation may or may not be present.
+  # The test is listed but does not cause a failure.
+  #
+  # Add here so we have something to alias later...
+  def skip desc = "skipped", &block
+    it desc do
+      skip desc
+    end
+  end unless respond_to?(:skip)
 
-    ##
-    # Define a named expectation to be skipped for now.
-    # The implementation may or may not be present.
-    # The test is listed but does not cause a failure.
-    #
-    # Add here so we have something to alias later...
-    def skip desc = "skipped", &block
-      it desc do
-        skip desc
-      end
-    end unless respond_to?(:skip)
+  alias :should_eventually :skip
 
-    alias :should_eventually :skip
-  end
 end
